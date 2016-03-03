53317cc94bb5d3a997352de64e809b5243e32b3c
Priit Liivak <priitliivak@gmail.com>
2016-03-03 11:10:25 +0200

    Create nortal-logo.sh

---
+++ nortal-logo.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Source: https://github.com/iriiiina/scripts/blob/master/nortal-logo.sh
+# Modified by: Priit Liivak
+# A more slim version of Nortal Logo
+# Prints picture of Nortal (www.nortal.com) logo
+# v0.1
+
+GREEN="\e[32m"
+NONE="\e[0m"
+
+printf "$GREEN\n"
+printf "      ||        ||\n"
+printf "     ||||      ||||\n"
+printf "     ||||||    ||||\n"
+printf "     ||||||||  ||||\n"
+printf "     ||||||||||||||\n"
+printf "     ||||||||||||||\n"
+printf "     ||||  ||||||||\n"
+printf "     ||||    ||||||\n"
+printf "     ||||      ||||\n"
+printf "      ||        || \n\n"
+printf "$NONE"
